f0552841938e39c2e31348efa7e6fbd4d16b3f4a
Juozas Auskalnis <j.auskalnis@gmail.com>
2019-06-08 16:47:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// Appss
+// Apps
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world latest ME! wednesday\n');
